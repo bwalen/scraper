2fed17c042880e8baf71f2ef2a78f06743fb5243
bwalen <bwalen@hotmail.com>
2016-06-04 16:36:43 -0700

    #1 Scraper will now be able to pull all text and links from the specified webpage and console log it.

--- scraper.js
+++ scraper.js
@@ -7,10 +7,16 @@ request(url, function(error, response, body){
   if(!error){
     var $ = cheerio.load(body);
     var linkElements = [];
+    var text = [];
     $("a").each(function(i, element){
       linkElements[i] = $(this).attr("href");
     });
+    $("*").each(function(i, element){
+      text[i] = $(this).text();
+
+    })
 
     console.log(linkElements);
+    console.log(text);
   }
 })
