7f75a5d5aa547be879b4eea9c8ae8574b90acff5
bwalen <bwalen@hotmail.com>
2016-06-09 22:26:37 -0700

    #1 Updated scraper method to just check certain types of elements and filter out unneeded text

--- scraper.js
+++ scraper.js
@@ -1,10 +1,10 @@
 var request = require("request");
 var cheerio = require("cheerio");
 var _ = require("./underscore.js");
+var _string = require("./underscore.string.min.js");
 
-var url = "http://www.reddit.com";
+var url = "http://www.foxnews.com";
 
-getText(url);
 getLinks(url);
 
 function getText(url){
@@ -12,12 +12,15 @@ function getText(url){
   request(url, function(error, response, body){
     if(!error){
       var $ = cheerio.load(body);
-      $("*").each(function(i, element){
-        text[i] = $(this).text();
+      $("p, li, ul, ol, span, article, blockquote, h1, h2, h3, h4, h5, h6").each(function(i, element){
+        if($(this).text().length > 12){
+          text[i] = $(this).text();
+        }
       })
     }
+    console.log(_string.clean(_.uniq(text).join(" ")));
   })
-  return text;
+  //return text;
 }
 
 function getLinks(url){
@@ -32,8 +35,8 @@ function getLinks(url){
         else{
           linkElements[i] = url + $(this).attr("href");
         }
+        linkElements = _.uniq(linkElements);
       });
     }
-  })
-return linkElements;
+  });
 }
